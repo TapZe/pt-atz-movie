c614c29998c4d80149bbb1ce3588cc1dabf122aa
TapZe <nabilmuyassar@Gmail.com>
2024-11-17 12:27:35 +0800

    feat: home controller for dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,9 +13,13 @@ class HomeController extends Controller
 {
     function home()
     {
-        $cinemas = Cinema::with('auditorium')->get();
+        $cinemas = Cinema::with([
+            'auditorium' => function ($query) {
+                $query->orderBy('name');
+            }
+        ])->get();
         $promos = Promo::all();
-        $distinctMovieIds = MovieSchedule::whereBetween('date', [Carbon::now(), Carbon::now()->addDays(7)])
+        $distinctMovieIds = MovieSchedule::whereBetween('date', [Carbon::now()->addDays(-1), Carbon::now()->addDays(7)])
             ->pluck('movie_id')
             ->unique();
         $movies = Movie::with(['genre'])->whereIn('id', $distinctMovieIds)->orderByDesc('release_date')->get();
@@ -28,12 +32,36 @@ class HomeController extends Controller
         ]);
     }
 
-    function movie(int $id)
+    function movie(string $id)
     {
         $movie = Movie::with('genre')->findOrFail($id);
-        $schedule = MovieSchedule::with(['auditorium.cinema']) // Eager load auditorium and cinema
-            ->whereBetween('date', [Carbon::now(), Carbon::now()->addDays(7)])
+        $schedule = MovieSchedule::with(['auditorium.cinema'])
+            ->whereBetween('date', [Carbon::now()->addDays(-1), Carbon::now()->addDays(7)])
             ->where('movie_id', $id)
+            ->orderBy('date')
             ->get();
+
+        return view('movie.detail', [
+            'movie' => $movie,
+            'schedule' => $schedule,
+        ]);
+    }
+
+    function cinema(string $id)
+    {
+        $cinema = Cinema::with([
+            'auditorium' => function ($query) {
+                $query->orderBy('name');
+            },
+            'auditorium.movieSchedule' => function ($query) {
+                $query->orderBy('date')->orderBy('show_start');
+                $query->whereBetween('date', [Carbon::now()->addDays(-1), Carbon::now()->addDays(7)]);
+            },
+            'auditorium.movieSchedule.movie'
+        ])->findOrFail($id);
+
+        return view('cinema.detail', [
+            'cinema' => $cinema,
+        ]);
     }
 }
